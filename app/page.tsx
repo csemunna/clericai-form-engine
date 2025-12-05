eb70076a3cc45cb744c734e046f0932d42c998d8
Mehedi Hasan <csemunnabd@gmail.com>
2025-12-04 21:25:08 -0500

    Redirect root to demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-"use client";
-
-import DemoPage from "./demo/page";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return <DemoPage />;
+  redirect("/demo");
 }
